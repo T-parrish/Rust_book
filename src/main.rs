9bab660937591ae7c8c02207be2a8c54fb07171c
Taylor Parrish <twpearish@gmail.com>
2020-03-12 15:51:36 -0700

    notes on for, while, loop, iterators, other good stuff

--- src/main.rs
+++ src/main.rs
@@ -104,7 +104,49 @@ fn main() {
 
     let swag = another_function(5);
 
-    println!("result from another_function: {}", swag)
+    println!("result from another_function: {}", swag);
+
+    let mut counter = 0;
+
+    // Loop syntax that increments the above counter on every iteration
+    // stores the result of the break statement in result from let statement
+    let result = loop {
+        counter += 1;
+
+        // Breaks the loop when counter == 10
+        if counter == 10 {
+            // returns counter * 2 (should be 20)
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+
+    let mut number = 3;
+
+    // while expression syntax decrementing by 1 on each increment
+    // breaks when the condition evaluates to a boolean false
+    while number != 0 {
+        println!("{}!", number);
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    let iter_array: [i32; 6] = [10, 20, 30, 40, 50, 80];
+
+    // expression that iterates over every element in an iterable slice
+    for el in iter_array.iter() {
+        println!("value is: {}", el)
+    }
+
+    // iterates over a reversed range a->(b-1)
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+
+
 }
 
 // Functions can take parameters or arguments
@@ -124,6 +166,20 @@ fn another_function(x: i32) -> u8 {
     };
 
     println!("Y value from expression: {}", y);
+
+    // If expressions expect a boolean Type
+    // Rust does not convert non-boolean types to boolean types like Javascript
+    // eg: if y { println!("{}", y) } doesn't work, since Rust expects a boolean
+    if y < 7 {
+        println!("{} is less than 7", y)
+    } else {
+        println!("{} is greater than 7", y)
+    }
+
+    // Can use if expressions in let statements
+    let x = if y < 7 { y } else { 0 };
+
+    println!("value of conditional x is: {}", x);
 
     // same as "return y" in Python
     y
