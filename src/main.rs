a38ee9da101bd4d200bc8ee1507f655f66b53821
Taylor Parrish <twpearish@gmail.com>
2020-03-13 17:48:14 -0700

    notes on ownership, Strings, slices

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,12 @@ fn main() {
     println!("result from another_function: {}", swag);
 
     iterators();
+    ownership();
+
+    let test_string = String::from("Booyakasha, fire pon the selectah");
+    let first_word = slices(&test_string);
+
+    println!("Original sentence: {} \nLength of 1st word: {}", test_string, first_word)
 }
 
 fn variable() {
@@ -191,4 +197,82 @@ fn iterators() {
         println!("{}!", number);
     }
     println!("LIFTOFF!!!");
+}
+
+fn ownership() {
+    // Make mutable variable and assign it to String
+    let mut x = String::from("Hello");
+
+    println!("X before updating: {}", x);
+
+    // Need to pass a mutable reference to x into the function since
+    // references are immutable by default in Rust
+    let (a, b, c) = add_to_string(&mut x);
+
+    println!("X after updating: {}", x);
+    println!(
+        "Length of x: {} \nLength of original string: {} \noriginal string: {}",
+        a, b, c
+    );
+
+    // Creates a scope block
+    {
+        // Sets r1 to be a reference to x
+        let r1 = &x;
+        // since you can only have one reference to a variable at a time
+        // and you can only have one mutable OR immutable reference
+        // we would be unable to do the following:
+        // let r2 = &x;
+        // let r2 = &mut x;
+
+        // Same applies for variable scoping, this variable is available
+        // within the scope block, but will be released when the scope block terminates
+        // unless it is returned somehow
+        let scoped_string = String::from("Badman Tings");
+
+        println!("Reference to r1: {}", r1);
+        println!("Block scoped string: {}", scoped_string);
+    } // once scope block terminates, the reference is released
+
+    // Can reuse this variable since the ownership of 'scoped_string'
+    // was released with the termination of the previous scope block
+    let scoped_string = String::from("Junglist Massive");
+    println!("Newly scoped string: {}", scoped_string);
+
+    // We can now set r2 to be a reference to x again.
+    let r2 = &x;
+    println!("Reference to r2: {}", r2);
+}
+
+fn add_to_string(input_string: &mut String) -> (usize, usize, String) {
+    // Need to use .clone() method to create a deep copy of the input variable
+    // Used when you want the heap data, not just the stack data
+    // let orig = input_string; makes a shallow copy and dereferences input_string
+    // Otherwise explained as: input_string would be 'moved' into orig
+    let orig = input_string.clone();
+
+    // extend the input String with string literal
+    input_string.push_str(", world");
+
+    // Can return values in a tuple
+    // Returns the length of the input string, orig length, and orig string
+    (input_string.len(), orig.len(), orig)
+}
+
+fn slices(s: &String) -> usize {
+    // Since you can't iterate over a String directly,
+    // create a bytes array
+    let bytes = s.as_bytes();
+
+    // create an iterator over the bytes array, and enumerate it
+    // enumerate returns a tuple (index, reference to item)
+    for (i, &item) in bytes.iter().enumerate() {
+        // find the first empty space and return the index
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    // base case: return length of string if there are no spaces
+    s.len()
 }
